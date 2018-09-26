195e1fb3327ca86b42ee8b213974d621cc04e922
lavanyaharinarayan <lharinarayan@berkeley.edu>
2018-09-25 19:46:38 -0700

    basic parser of Gutenberg files

---
+++ extract.py
@@ -0,0 +1,43 @@
+from abc import ABCMeta, abstractmethod, abstractproperty
+import requests
+
+from bs4 import BeautifulSoup
+
+class Extract:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def parse(self):
+        pass
+
+
+class GutenbergPlaintext(Extract):
+	start = "*** START OF THIS PROJECT GUTENBERG EBOOK "
+	end = "*** END OF THIS PROJECT GUTENBERG EBOOK"
+
+	def __init__(self, link):
+		self.link = link
+
+	def _extract_text(self):
+		html = requests.get(self.link).content
+		start_index = self.start
+
+		soup = BeautifulSoup(html, features="html.parser")
+		for script in soup(["script", "style"]):
+		    script.extract()    # rip it out
+
+		text = soup.body.get_text()
+
+		start_index = text.find(self.start)+len(self.start)
+		end_index = text.find(self.end)
+
+		return text[start_index:end_index]
+
+	def _split(self, text):
+		# Split paragraphs
+		paragraph = "\n\n"
+		return text.split(paragraph)
+
+	def parse(self):
+		return self._split(self._extract_text())
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from extract import GutenbergPlaintext
+
+x = GutenbergPlaintext("https://www.gutenberg.org/files/84/84-h/84-h.htm").parse()
